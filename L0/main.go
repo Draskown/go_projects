e9c847baa3962c70e47bd23adeafac259e01c98b
Draskown <andrey2000534@gmail.com>
2023-11-10 10:49:47 +0300

    Moved server to a goroutine

--- L0/main.go
+++ L0/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -15,7 +18,7 @@ import (
 	"github.com/Draskown/WBL0/server/service"
 )
 
-func main ()  {
+func main() {
 	// Set formatter for logrus
 	logrus.SetFormatter(new(logrus.JSONFormatter))
 	// Find Viper config
@@ -29,11 +32,11 @@ func main ()  {
 
 	// Create a struct for database from config info
 	db, err := repository.NewPostgresDB(repository.DBConfig{
-		Host: viper.GetString("postgres.host"),
-		Port: viper.GetString("postgres.port"),
+		Host:     viper.GetString("postgres.host"),
+		Port:     viper.GetString("postgres.port"),
 		Username: viper.GetString("postgres.username"),
-		DBName: viper.GetString("postgres.dbname"),
-		SSLMode: viper.GetString("postgres.sslmode"),
+		DBName:   viper.GetString("postgres.dbname"),
+		SSLMode:  viper.GetString("postgres.sslmode"),
 		// Get password from environment variable
 		Password: os.Getenv("DB_PASSWORD"),
 	})
@@ -42,14 +45,27 @@ func main ()  {
 	}
 
 	repo := repository.NewRepository(db)
-	service := service.NewService(repo) 
+	service := service.NewService(repo)
 	hnd := handler.NewHandler(service)
-	
+
 	srv := new(server.Server)
 
-	// Launch the server with a port from Viper config
-	if err := srv.Run(viper.GetString("port"), hnd.InitRoutes()); err != nil {
-		logrus.Fatalf("Could not run the server (%s)\n", err.Error())
+	go func() {
+		// Launch the server with a port from Viper config
+		if err := srv.Run(viper.GetString("port"), hnd.InitRoutes()); err != nil {
+			logrus.Fatalf("Could not run the server (%s)\n", err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Fatalf("Could not shutdown the server (%s)", err.Error())
+	}
+	if err := db.Close(); err != nil {
+		logrus.Fatalf("Could not close database connection (%s)", err.Error())
 	}
 }
 
