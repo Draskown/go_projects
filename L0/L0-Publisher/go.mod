fe507e06598d524531bdf64c87cddd1b16ba8ed9
Draskown <andrey2000534@gmail.com>
2023-11-19 11:46:53 +0300

    Added STAN Publisher

---
+++ L0/L0-Publisher/go.mod
@@ -0,0 +1,15 @@
+module github.com/Draskown/WBL0-Publisher
+
+go 1.21.1
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/nats-io/nats.go v1.31.0 // indirect
+	github.com/nats-io/nkeys v0.4.6 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/nats-io/stan.go v0.10.4 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
---
+++ L0/L0-Publisher/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	stan "github.com/nats-io/stan.go"
+	"github.com/sirupsen/logrus"
+)
+
+// Stan connection configuration
+type StanCfg struct {
+	ClusterName string
+	ClientName  string
+	SubjectName string
+}
+
+// Stan configuration constructor
+//
+// Accepts string values got from environment variables
+func NewStanCfg(clun, clin, subn string) *StanCfg {
+	return &StanCfg{
+		ClusterName: clun,
+		ClientName:  clin,
+		SubjectName: subn,
+	}
+}
+
+func main() {
+	// Create new STAN configuration
+	// from environment variables
+	stanCfg := NewStanCfg(
+		os.Getenv("CLUSTER_NAME"),
+		os.Getenv("CLIENT_NAME"),
+		os.Getenv("SUBJECT_NAME"),
+	)
+
+	// Connect to STAN
+	sc, err := stan.Connect(stanCfg.ClusterName, stanCfg.ClientName+"_pub")
+	if err != nil {
+		logrus.Fatalf("Error while connecting to STAN in Publisher (%s)\n", err.Error())
+		return
+	}
+
+	// Open model file
+	file, err := os.Open("model.json")
+	if err != nil {
+		logrus.Fatalf("Error while opening 'model.json' file in Publisher (%s)\n", err.Error())
+		return
+	}
+	defer file.Close()
+
+	// Watches for the file to change
+	go func() {
+		err := watchFile(file, sc, stanCfg.SubjectName)
+		if err != nil {
+			logrus.Fatalf("Error while reading from 'model.json' file in Publisher (%s)\n", err.Error())
+		}
+	}()
+
+	// Waits for the Ctrl+C signal to suspend the main goroutine
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	// Close the STAN connection
+	if err := sc.Close(); err != nil {
+		logrus.Fatalf("Error while closing STAN connection in Publisher (%s)\n", err.Error())
+	}
+}
+
+// Watches for the file's change in the infinite loop
+// and sends its contents to the STAN subject
+//
+// So every time 'model.json' is changed, the message is sent
+func watchFile(file *os.File, sc stan.Conn, subName string) error {
+	// Get initial file statistics
+	initialStat, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	for {
+		// Get current file statistics
+		stat, err := file.Stat()
+		if err != nil {
+			return err
+		}
+
+		// If current statistics is different from the initial
+		if stat.Size() != initialStat.Size() || stat.ModTime() != initialStat.ModTime() {
+			bytes, err := io.ReadAll(file)
+			if err != nil {
+				fmt.Println(err.Error())
+				return err
+			}
+
+			// Publish the changed file contents
+			sc.Publish(subName, bytes)
+
+			// Update initial statistics the new ones
+			initialStat = stat
+		}
+
+		// Return to the start of the file
+		_, err = file.Seek(0, io.SeekStart)
+		if err != nil {
+			return err
+		}
+
+		// One second delay
+		time.Sleep(1 * time.Second)
+	}
+}
